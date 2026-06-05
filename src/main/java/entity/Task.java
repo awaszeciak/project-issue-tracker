97bc9ed859adacffdf3256693992360986de99a9
awaszeciak <a.waszeciak@gmail.com>
2026-06-05 22:06:44 +0200

    Add task entity

---
+++ src/main/java/entity/Task.java
@@ -0,0 +1,41 @@
+package entity;
+
+
+import enums.Priority;
+import enums.TaskStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Priority priority;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+}
