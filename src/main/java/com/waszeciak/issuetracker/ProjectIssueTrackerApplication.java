ec1dcf45568fe9195dd2c4215b19b40e4486b16b
awaszeciak <a.waszeciak@gmail.com>
2026-06-05 21:39:26 +0200

    Initialize Spring Boot project

---
+++ src/main/java/com/waszeciak/issuetracker/ProjectIssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.waszeciak.issuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectIssueTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectIssueTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/waszeciak/issuetracker/ProjectIssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waszeciak.issuetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectIssueTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
